d170a8c8f3975cc82d7cc4e1df5f8c02dc485421
FordangIT <fordang0819@gmail.com>
2024-01-22 21:45:04 +0900

    chore: 기본 세팅

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="w-20 h-20 bg-red-300">안녕</div>
+    </main>
+  );
+}
