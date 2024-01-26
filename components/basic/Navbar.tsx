acd1e89fd00cb1a50049749dbbda802defcded35
FordangIT <fordang0819@gmail.com>
2024-01-26 17:59:46 +0900

    feat: 기본 css 진행중

--- components/basic/Navbar.tsx
+++ components/basic/Navbar.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+
 export default function Navbar() {
-  return <>Navbar</>;
+  return (
+    <>
+      <div className="bg-main-pink h-40 flex justify-center items-center">
+        <div className="mx-80 flex w-2/3 h-16">
+          <Image
+            src="/logo.png"
+            alt="logo of artgarden"
+            width={180}
+            height={20}
+          ></Image>
+          <div className="flex ml-10 items-center justify-between font-semibold text-xl text-white w-72">
+            <div>공연</div>
+            <div>전시</div>
+            <div>팝업스토어</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
