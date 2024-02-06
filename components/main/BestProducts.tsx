835221863343100419bde7abadfdb1b7dc638044
FordangIT <fordang0819@gmail.com>
2024-02-06 12:48:48 +0900

    [AG-67]Feat: best 공연 랭킹 불러오기 완료

--- components/main/BestProducts.tsx
+++ components/main/BestProducts.tsx
@@ -5,7 +5,7 @@ const BestProducts = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/api/performances");
+        const response = await fetch("/api/performances/best");
         if (!response.ok) {
           throw new Error("failed to fetch data");
         }
@@ -19,30 +19,35 @@ const BestProducts = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
-      {data &&
-        data.map((el) => (
-          <div
-            key={el.id}
-            className="card w-[26rem] h-[30rem] bg-white shadow-xl rounded-none border-2 border-white"
-          >
-            <figure>
-              <img src={el.img} alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                {el.name}
-                <div className="badge bg-main-pink text-white">BEST</div>
-              </h2>
-              가격: {el.price}원 <p>장소: {el.place}</p>
-              <div className="card-actions justify-end">
-                {el.end} <div className="badge badge-outline">{el.genre}</div>
-                <div className="badge badge-outline">{el.rank}</div>
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
+        {data &&
+          data.map((el) => (
+            <div
+              key={el.id}
+              className="card w-[26rem] h-[30rem] bg-white shadow-xl rounded-none border-2 border-white"
+            >
+              <figure>
+                <Image src={el.img} alt="공연사진" width={420} height={450} />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  {el.name}
+                  <div className="badge bg-main-pink text-white">BEST</div>
+                </h2>
+                공연기간: {el.date} <p>지역: {el.place}</p>
+                <div className="card-actions justify-end">
+                  <div className="badge badge-outline">{el.genre}</div>
+                  <div className="badge badge-outline">
+                    공연횟수: {el.count}번
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-    </div>
+          ))}
+      </div>
+      <div>더 보기</div>
+    </>
   );
 };
 export default BestProducts;
---
+++ pages/api/performances/best.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+import { parseString } from "xml2js";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const kopis_key = process.env.KOPIS_KEY;
+  try {
+    const response = await axios.get(
+      `http://kopis.or.kr/openApi/restful/boxoffice?service=${kopis_key}&ststype=week&date=20240121`
+    );
+    const xmlData = response.data;
+    parseString(xmlData, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: "failed to parse XML" });
+      }
+      const jsonData = result.boxofs?.boxof?.map((item) => ({
+        id: item.mt20id?.[0],
+        name: item.prfnm?.[0],
+        img: "http://www.kopis.or.kr" + item.poster?.[0],
+        date: item.prfpd?.[0],
+        place: item.area?.[0],
+        genre: item.cate?.[0],
+        count: item.prfdtcnt?.[0],
+      }));
+      if (!jsonData) {
+        return res.status(500).json({ error: "data format error" });
+      }
+      return res.status(200).json(jsonData);
+    });
+  } catch (error) {
+    console.log("error");
+    res.status(500).json({ error: "internal server error" });
+  }
+}
