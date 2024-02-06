e002b2a5483048e51ad0c184f58894c1627420cc
FordangIT <fordang0819@gmail.com>
2024-02-06 18:00:07 +0900

    [AG-78]Design: css 수정

--- components/main/BestProducts.tsx
+++ components/main/BestProducts.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 const BestProducts = () => {
+  const truncateText = (text: string, maxLength: number) => {
+    return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
+  };
   const [data, setData] = useState<Array<any>>([]);
   useEffect(() => {
     const fetchData = async () => {
@@ -28,11 +31,11 @@ const BestProducts = () => {
               className="card w-[26rem] h-[30rem] bg-white shadow-xl rounded-none border-2 border-white"
             >
               <figure>
-                <Image src={el.img} alt="공연사진" width={420} height={450} />
+                <Image src={el.img} alt="공연사진" width={420} height={380} />
               </figure>
               <div className="card-body">
                 <h2 className="card-title">
-                  {el.name}
+                  {truncateText(el.name, 16)}
                   <div className="badge bg-main-pink text-white">BEST</div>
                 </h2>
                 공연기간: {el.date} <p>지역: {el.place}</p>
