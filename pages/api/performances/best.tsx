e785e2455c47dbdccf8308cb4837a3cc7489fe3b
FordangIT <fordang0819@gmail.com>
2024-02-08 11:51:39 +0900

    feat: api 수정

--- pages/api/performances/best.tsx
+++ pages/api/performances/best.tsx
@@ -2,13 +2,23 @@ import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 import { parseString } from "xml2js";
 
+interface BestProducts {
+  id?: string;
+  name?: string;
+  img?: string;
+  date?: string;
+  place?: string;
+  genre?: string;
+  count?: string;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-) {
+): Promise<void> {
   const kopis_key = process.env.KOPIS_KEY;
   try {
-    const response = await axios.get(
+    const response = await axios.get<string>(
       `http://kopis.or.kr/openApi/restful/boxoffice?service=${kopis_key}&ststype=week&date=20240121`
     );
     const xmlData = response.data;
@@ -17,15 +27,17 @@ export default async function handler(
         console.log(err);
         return res.status(500).json({ error: "failed to parse XML" });
       }
-      const jsonData = result.boxofs?.boxof?.map((item) => ({
-        id: item.mt20id?.[0],
-        name: item.prfnm?.[0],
-        img: "http://www.kopis.or.kr" + item.poster?.[0],
-        date: item.prfpd?.[0],
-        place: item.area?.[0],
-        genre: item.cate?.[0],
-        count: item.prfdtcnt?.[0],
-      }));
+      const jsonData: BestProducts[] = result.boxofs?.boxof?.map(
+        (item: any) => ({
+          id: item.mt20id?.[0],
+          name: item.prfnm?.[0],
+          img: "http://www.kopis.or.kr" + item.poster?.[0],
+          date: item.prfpd?.[0],
+          place: item.area?.[0],
+          genre: item.cate?.[0],
+          count: item.prfdtcnt?.[0],
+        })
+      );
       if (!jsonData) {
         return res.status(500).json({ error: "data format error" });
       }
--- pages/api/performances/new.tsx
+++ pages/api/performances/new.tsx
@@ -2,13 +2,24 @@ import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 import { parseString } from "xml2js";
 
+interface PerformanceData {
+  id: string;
+  name: string;
+  img: string;
+  start: string;
+  end: string;
+  place: string;
+  genre: string;
+  rank: string;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-) {
+): Promise<void> {
   const kopis_key = process.env.KOPIS_KEY;
   try {
-    const response = await axios.get(
+    const response = await axios.get<string>(
       `http://www.kopis.or.kr/openApi/restful/pblprfr?service=${kopis_key}&stdate=20160101&eddate=20241231&cpage=1&rows=100&prfstate=02&signgucode=11&signgucodesub=1111&kidstate=Y&newsql=Y`
     );
     const xmlData = response.data;
@@ -18,7 +29,7 @@ export default async function handler(
         console.log(err);
         return res.status(500).json({ error: "failed to parse XML" });
       }
-      const jsonData = result.dbs.db.map((item) => ({
+      const jsonData: PerformanceData[] = result.dbs.db.map((item: any) => ({
         id: item.mt20id[0],
         name: item.prfnm[0],
         img: item.poster[0],
@@ -33,7 +44,7 @@ export default async function handler(
       }
 
       const sortedData = jsonData.sort(
-        (a, b) => new Date(b.start) - new Date(a.start)
+        (a, b) => new Date(b.start).getTime() - new Date(a.start).getTime()
       );
       const paginatedData = sortedData.slice(0, 8);
       return res.status(200).json(paginatedData);
