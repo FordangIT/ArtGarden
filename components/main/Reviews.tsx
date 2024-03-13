e8832d8f4053cd24dc694c2c119e19dfdafae22b
FordangIT <fordang0819@gmail.com>
2024-03-13 17:42:40 +0900

    [AG-100]Feat: 백엔드 리뷰 데이터 연결

--- components/main/Reviews.tsx
+++ components/main/Reviews.tsx
@@ -1,87 +1,48 @@
 import Image from "next/image";
+import { useQuery } from "react-query";
+import axios, { AxiosResponse } from "axios";
+
+interface Review_Data {
+  id: string;
+  perform_id: string;
+  content: string;
+  rate: number;
+  member_id: number;
+  created_at: string;
+  modified_at: string;
+}
 const Reviews = () => {
-  const data = [
-    {
-      id: 1,
-      name: "제목입니당",
-      user: "사용자 이름",
-      img: "https://picsum.photos/420/300",
-      star: "3",
-      content: "평범했어요!",
-    },
-    {
-      id: 2,
-      name: "이름입니당",
-      user: "사용자 이름",
-      img: "https://picsum.photos/420/300",
-      star: "2023년 12월 3일",
-      content: "",
-    },
-    {
-      id: 3,
-      name: "이름입니당",
-      user: "사용자 이름",
-      img: "https://picsum.photos/420/300",
-      star: "2023년 12월 3일",
-      content: "",
-    },
-    {
-      id: 4,
-      name: "이름입니당",
-      user: "사용자 이름",
-      img: "https://picsum.photos/420/300",
-      star: "2023년 12월 3일",
-      content: "",
-    },
-    {
-      id: 5,
-      name: "이름입니당",
-      user: "사용자 이름",
-      img: "https://picsum.photos/420/300",
-      star: "2023년 12월 3일",
-      content: "",
-    },
-    {
-      id: 6,
-      name: "이름입니당",
-      user: "사용자 이름",
-      img: "https://picsum.photos/420/300",
-      star: "2023년 12월 3일",
-      content: "",
-    },
-    {
-      id: 7,
-      name: "이름입니당",
-      user: "사용자 이름",
-      img: "https://picsum.photos/420/300",
-      star: "2023년 12월 3일",
-      content: "",
-    },
-    {
-      id: 8,
-      name: "이름입니당",
-      user: "사용자 이름",
-      img: "https://picsum.photos/420/300",
-      star: "2023년 12월 3일",
-      content: "",
-    },
-  ];
+  const { data, isLoading, isError, error } = useQuery<Review_Data[]>(
+    "reviewData",
+    async () => {
+      const res: AxiosResponse<Review_Data[]> = await axios.get(
+        "http://54.180.145.33:8080/reviews"
+      );
+      return res.data;
+    }
+  );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>{(error as Error).message}</div>;
+  }
   return (
     <div className="flex justify-center ">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
         {data &&
-          data.map((el) => (
+          data?.map((el) => (
             <div
               key={el.id}
               className="card w-80 h-96 bg-white shadow-xl rounded-xl border-2 border-white"
             >
               <figure>
-                <img src={el.img} alt="review-image" />
+                <img src={el.perform_id} alt="review-image" />
               </figure>
               <div className="card-body">
-                <h2 className="card-title">{el.name}</h2>
-                <h3>사용자: {el.user}</h3>
-                <h3>별점: {el.star}</h3>
+                <h2 className="card-title">{el.perform_id}</h2>
+                <h3>사용자: {el.member_id}</h3>
+                <h3>별점: {el.rate}</h3>
                 <h3>내용: {el.content}</h3>
               </div>
             </div>
