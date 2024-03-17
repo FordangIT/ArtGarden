9549149ea7997fff0b79cfc95c594da7e612edcb
FordangIT <fordang0819@gmail.com>
2024-03-17 20:51:59 +0900

    [AG-84]Feat: 전체 공연 정보 페이지

--- pages/performances/index.tsx
+++ pages/performances/index.tsx
@@ -1,10 +1,13 @@
-import AllProducts from "@/components/performances/AllProducts";
 import SearchCondition from "@/components/performances/SearchCondition";
+import AllPerformances from "@/components/performances/AllPerformances";
+// import Test from "@/components/performances/test";
+
+//공연 전체 정보 페이지
 function Performances() {
   return (
     <div className="flex-col">
+      <AllPerformances />
       <SearchCondition />
-      <AllProducts />
     </div>
   );
 }
