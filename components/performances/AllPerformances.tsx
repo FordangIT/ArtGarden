5865e3e654b4a1ba4564aaeff38250abd0a0271d
FordangIT <fordang0819@gmail.com>
2024-03-26 19:13:51 +0900

    [AG-90]Feat: 초성 검색, 단어 검색 기능

--- components/performances/AllPerformances.tsx
+++ components/performances/AllPerformances.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { useInfiniteQuery } from "react-query";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import axios from "axios";
 import { useObserver } from "@/customHook/useObserver";
 import useLocalStorage from "use-local-storage";
-
+import { BiSearch } from "react-icons/bi";
+import Hangul from "hangul-js";
 interface Performance {
   id: string;
   name: string;
@@ -25,6 +26,11 @@ interface QueryData {
 const AllPerformances: React.FC = () => {
   const [scrollY, setScrollY] = useLocalStorage("performance_scroll", 0);
   const bottom = useRef(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event?.target.value);
+  };
   const truncateText = (text: string, maxLength: number) => {
     return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
   };
@@ -64,9 +70,23 @@ const AllPerformances: React.FC = () => {
     onIntersect,
   });
 
+  const getInitials = (text: string, text2: string) => {
+    let searcher = new Hangul.Searcher(text);
+    return searcher.search(text2);
+  };
   return (
     <>
-      <div className="flex items-center justify-center">
+      <div className="flex-col items-center justify-center">
+        <div className="flex items-center justify-center ">
+          <input
+            type="text"
+            placeholder="공연 이름을 검색하세요"
+            value={searchTerm}
+            onChange={handleSearch}
+            className="border-2 border-slate-300 rounded-3xl w-[40rem] h-12 mb-12 px-10"
+          />
+        </div>
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
           {status === "loading" && <p>불러오는 중</p>}
           {status === "error" && <p>불러오기 실패</p>}
@@ -75,42 +95,51 @@ const AllPerformances: React.FC = () => {
               {data?.pages?.map((page) => {
                 const performanceList = page.data.data;
                 return performanceList.map((el: Performance) => {
-                  return (
-                    <Link
-                      href={`/performances/${el.id}`}
-                      key={el.id}
-                      onClick={() => setScrollY(window.scrollY)}
-                    >
-                      <div className="card w-[26rem] h-[30rem] bg-white shadow-xl rounded-none border-2 border-white  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-100">
-                        <figure>
-                          <Image
-                            src={el.posterUrl}
-                            alt="공연사진"
-                            width={420}
-                            height={380}
-                          />
-                        </figure>
-                        <div className="card-body">
-                          <h2 className="card-title">
-                            {truncateText(el.name, 16)}
-                            <div className="badge bg-main-pink text-white">
-                              BEST
-                            </div>
-                          </h2>
-                          공연기간: {el.startDate}~ {el.endDate}{" "}
-                          <p>지역: {truncateText(el.place, 16)}</p>
-                          <div className="card-actions justify-end">
-                            <div className="badge badge-outline">
-                              장르/ 공연상태
-                            </div>
-                            <div className="badge badge-outline">
-                              {truncateText(el.price, 10)}
+                  const searchKeywordInitials = getInitials(
+                    searchTerm,
+                    el.name
+                  );
+                  if (
+                    searchKeywordInitials === 0 ||
+                    el.name.toLowerCase().includes(searchTerm.toLowerCase())
+                  ) {
+                    return (
+                      <Link
+                        href={`/performances/${el.id}`}
+                        key={el.id}
+                        onClick={() => setScrollY(window.scrollY)}
+                      >
+                        <div className="card w-[26rem] h-[30rem] bg-white shadow-xl rounded-none border-2 border-white  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-100">
+                          <figure>
+                            <Image
+                              src={el.posterUrl}
+                              alt="공연사진"
+                              width={420}
+                              height={380}
+                            />
+                          </figure>
+                          <div className="card-body">
+                            <h2 className="card-title">
+                              {truncateText(el.name, 16)}
+                              <div className="badge bg-main-pink text-white">
+                                BEST
+                              </div>
+                            </h2>
+                            공연기간: {el.startDate}~ {el.endDate}{" "}
+                            <p>지역: {truncateText(el.place, 22)}</p>
+                            <div className="card-actions justify-end">
+                              <div className="badge badge-outline">
+                                장르/ 공연상태
+                              </div>
+                              <div className="badge badge-outline">
+                                {truncateText(el.price, 12)}
+                              </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                    </Link>
-                  );
+                      </Link>
+                    );
+                  }
                 });
               })}
             </>
