e0ef70229f001a60a11933d9a5de96d7ea2cb25e
FordangIT <fordang0819@gmail.com>
2024-04-03 20:14:42 +0900

    [AG-87]Feat: 백엔드 url env 설정

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ["www.kopis.or.kr"],
   },
+  env: {
+    KOPIS_URL: process.env.KOPIS_URL,
+    KOPIS_KEY: process.env.KOPIS_KEY,
+    BACKEND_URL: process.env.BACKEND_URL,
+  },
 };
 
 export default nextConfig;
