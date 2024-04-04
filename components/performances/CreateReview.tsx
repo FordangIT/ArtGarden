3304b2df4a351739398fc59868694fd59fab654b
FordangIT <fordang0819@gmail.com>
2024-04-04 19:50:10 +0900

    [AG-94]Feat: 리뷰 create 란 생성

---
+++ components/performances/CreateReview.tsx
@@ -0,0 +1,12 @@
+function CreateReview() {
+  return (
+    <>
+      <div>리뷰 작성</div>
+      <input></input>
+      <button>제출</button>
+      <div>별점</div>
+    </>
+  );
+}
+
+export default CreateReview;
