f74d1712bd7e491f7c5cb859457c58d6393fa397
FordangIT <fordang0819@gmail.com>
2024-04-14 15:23:20 +0900

    Error: 배포파일 수정

--- scripts/application-start.sh
+++
@@ -1,9 +0,0 @@
-  #!/bin/bash
-  pm2 describe artgarden > /dev/null
-  RUNNING=$?
-
-  if [ "${RUNNING}" -ne 0 ]; then
-    pm2 start npm --name "artgarden" -- start
-  else
-    pm2 reload artgarden
-  fi;
