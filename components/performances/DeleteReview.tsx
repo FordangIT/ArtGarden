932d9cc3d802a6442acb01378ba7836844ccade4
FordangIT <fordang0819@gmail.com>
2024-04-17 18:55:15 +0900

    Docs: 해당 코드 주석처리

--- components/performances/DeleteReview.tsx
+++ components/performances/DeleteReview.tsx
@@ -1,35 +1,35 @@
-import { useMutation } from "react-query";
+// import { useMutation } from "react-query";
 
-async function deleteReview(reviewId: number): Promise<any> {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/reviews/${reviewId}`,
-    {
-      method: "DELETE",
-    }
-  );
-  if (!res.ok) {
-    throw new Error("Failed to delete review");
-  }
-  return res.json();
-}
+// async function deleteReview(reviewId: number): Promise<any> {
+//   const res = await fetch(
+//     `${process.env.NEXT_PUBLIC_BACKEND_URL}/reviews/${reviewId}`,
+//     {
+//       method: "DELETE",
+//     }
+//   );
+//   if (!res.ok) {
+//     throw new Error("Failed to delete review");
+//   }
+//   return res.json();
+// }
 
-function DeleteReview({ reviewId }: { reviewId: number }) {
-  const mutation = useMutation(deleteReview);
+// function DeleteReview({ reviewId }: { reviewId: number }) {
+//   const mutation = useMutation(deleteReview);
 
-  const handleDelete = async () => {
-    try {
-      await mutation.mutateAsync(reviewId);
-      console.log("리뷰가 성공적으로 삭제되었습니다.");
-    } catch (error) {
-      console.error("리뷰 삭제에 실패했습니다:", error);
-    }
-  };
+//   const handleDelete = async () => {
+//     try {
+//       await mutation.mutateAsync(reviewId);
+//       console.log("리뷰가 성공적으로 삭제되었습니다.");
+//     } catch (error) {
+//       console.error("리뷰 삭제에 실패했습니다:", error);
+//     }
+//   };
 
-  return (
-    <button onClick={handleDelete} disabled={mutation.isLoading}>
-      {mutation.isLoading ? "삭제 중..." : "리뷰 삭제"}
-    </button>
-  );
-}
+//   return (
+//     <button onClick={handleDelete} disabled={mutation.isLoading}>
+//       {mutation.isLoading ? "삭제 중..." : "리뷰 삭제"}
+//     </button>
+//   );
+// }
 
-export default DeleteReview;
+// export default DeleteReview;
---
+++ components/performances/UpdateReview.tsx
@@ -0,0 +1,86 @@
+// import React, { useEffect, useState } from "react";
+// import { useMutation, useQueryClient } from "react-query";
+// import axios from "axios";
+// interface ReviewData {
+//   reviewid: number;
+//   content: string;
+//   rate: number;
+//   upid: string;
+// }
+
+// function UpdateReview({ reviewId }: { reviewId: number }) {
+//   const queryClient = useQueryClient();
+//   const [rate, setRate] = useState(5);
+//   const [reviewText, setReviewText] = useState("");
+
+//   const submitReview = async (reviewData: ReviewData) => {
+//     try {
+//       const res = await axios.post(
+//         `${process.env.NEXT_PUBLIC_BACKEND_URL}/reviews/${reviewId}`,
+//         reviewData
+//       );
+//       setReviewText("");
+//       setRate(1);
+//     } catch (error) {
+//       console.log("error", error);
+//       throw new Error("error fetching data");
+//     }
+//   };
+
+//   const mutation = useMutation(submitReview, {
+//     onSuccess: () => {
+//       queryClient.invalidateQueries("reviews");
+//       console.log("성공 mutation");
+//     },
+//     onError: (error) => {
+//       console.error("Mutation error", error); // mutation 에러 로그 출력
+//       // 에러 처리 로직 추가 (예: 사용자에게 알림 등)
+//     },
+//   });
+
+//   const handleRatingChange = (selectedRate: number) => {
+//     setRate(selectedRate);
+//     console.log(selectedRate, "clickstar");
+//   };
+
+//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     console.log(reviewId, rate, "아이디랑 별 점수");
+//     await mutation.mutate({
+//       reviewid: reviewId,
+//       content: reviewText,
+//       rate: Number(rate),
+//       upid: "1",
+//     });
+//   };
+
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <textarea
+//           value={reviewText}
+//           onChange={(e) => setReviewText(e.target.value)}
+//           placeholder="Write your review..."
+//           rows={4}
+//           cols={50}
+//         />
+//         <div className="rating">
+//           {[1, 2, 3, 4, 5].map((star, idx) => (
+//             <input
+//               key={idx}
+//               type="radio"
+//               name="rating"
+//               value={star}
+//               className="mask mask-star-2 bg-orange-400"
+//               onClick={() => handleRatingChange(star)} // rating 선택 시 handleRatingChange 호출
+//             />
+//           ))}
+//         </div>
+
+//         <button type="submit">수정하기</button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default UpdateReview;
