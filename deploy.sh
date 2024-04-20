942858a927df40e96cdfc604f3457489d834c9ab
FordangIT <fordang0819@gmail.com>
2024-04-20 14:41:52 +0900

    Error:적절한 node.js 버전 사용하도록 변경

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,9 @@ pm2 describe artgarden > /dev/null
 if [ $? -eq 0 ]; then
     # 실행 중인 경우
     echo "artgarden 프로세스가 이미 실행 중입니다."
-    pm2 reload 
+    pm2 reload artgarden
 else
     # 실행 중이 아닌 경우
     echo "artgarden 프로세스가 실행되지 않았습니다."
-    pm2 start npm --name "artgarden" -- start
+    pm2 start --interpreter none npm --name "artgarden" -- start
 fi
