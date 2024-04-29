7638de4e3ae1eee5b5457ed3cab39da6bfb1765d
FordangIT <fordang0819@gmail.com>
2024-04-29 21:50:56 +0900

    Docs: 준비중 page

--- pages/exhibitions/index.tsx
+++ pages/exhibitions/index.tsx
@@ -1,4 +1,15 @@
+import Image from "next/image";
 function Exhibition() {
-  return <>Exhibition</>;
+  return (
+    <div className="flex justify-center items-center my-44 ">
+      <Image
+        src="/readyPage.png"
+        alt="준비중 페이지"
+        width={100}
+        height={100}
+        className="w-96 h-96"
+      />
+    </div>
+  );
 }
 export default Exhibition;
