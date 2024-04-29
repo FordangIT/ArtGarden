9490caa8c6dd70c10fbb6b288d815affe1a75bce
FordangIT <fordang0819@gmail.com>
2024-04-29 17:07:52 +0900

    [AG-115]Design: nav 반응형

--- components/basic/Navbar.tsx
+++ components/basic/Navbar.tsx
@@ -1,34 +1,45 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaArrowDown } from "react-icons/fa";
+import Smallbar from "./Smallbar";
 export default function Navbar() {
   return (
     <>
-      <div className="bg-main-pink h-40 flex justify-center items-center">
-        <div className="mx-80 flex w-2/3 h-16 items-center">
-          <Link href="/">
+      <div className="bg-main-pink flex justify-center items-center h-40">
+        <div className="flex justify-between items-center w-2/3">
+          <Link href="/" className=" w-48 ">
             <Image
               src="/logo.png"
               alt="logo of artgarden"
-              width={180}
-              height={20}
+              width={200}
+              height={40}
             ></Image>
           </Link>
-          <div className="flex ml-10 mr-6 items-center justify-between font-semibold text-xl text-white w-72">
-            <Link href="/performances">
-              <div className="hover:text-main-yellow">공연</div>
-            </Link>
-            <Link href="exhibitions">
-              <div className="hover:text-main-yellow">전시</div>
-            </Link>
-            <Link href="popupstores">
-              <div className="hover:text-main-yellow">팝업스토어</div>
-            </Link>
+
+          <div className="lg:hidden">
+            <Smallbar />
           </div>
-          <div className="flex justify-around items-center ml-auto bg-black w-36 h-12 border-2 border-main-yellow rounded-2xl">
-            <div className="font-semibold text-xl text-main-yellow">Login</div>
-            <div className="bg-main-yellow rounded-full text-lg">
-              <FaArrowDown />
+          <div className="hidden lg:block">
+            <div className="flex">
+              <div className="flex items-center justify-between font-semibold text-2xl text-white w-72 ml-8">
+                <Link href="/performances">
+                  <div className="hover:text-main-yellow ">공연</div>
+                </Link>
+                <Link href="exhibitions">
+                  <div className="hover:text-main-yellow ">전시</div>
+                </Link>
+                <Link href="popupstores">
+                  <div className="hover:text-main-yellow">팝업스토어</div>
+                </Link>
+              </div>
+              <div className="flex justify-around items-center bg-black w-36 h-12 border-2 border-main-yellow rounded-2xl ml-12">
+                <div className="font-semibold text-xl text-main-yellow">
+                  Login
+                </div>
+                <div className="bg-main-yellow rounded-full text-lg">
+                  <FaArrowDown />
+                </div>
+              </div>
             </div>
           </div>
         </div>
