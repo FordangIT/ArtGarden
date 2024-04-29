1f55526b4e28fc9e316c11ebc45aa76da6f6b533
FordangIT <fordang0819@gmail.com>
2024-04-29 17:07:52 +0900

    [AG-115]Design: nav 반응형

---
+++ components/basic/Smallbar.tsx
@@ -0,0 +1,11 @@
+import { TfiAlignJustify } from "react-icons/tfi";
+
+function Smallbar() {
+  return (
+    <>
+      <TfiAlignJustify className="w-8 h-8" />
+    </>
+  );
+}
+
+export default Smallbar;
