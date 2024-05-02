a963a72556ac271c154c1326a25a7dc92126f97e
FordangIT <fordang0819@gmail.com>
2024-05-02 11:08:52 +0900

    [AG-115]Design: 반응형 수정

--- pages/performances/[id].tsx
+++ pages/performances/[id].tsx
@@ -52,90 +52,88 @@ function DetailPage(props: PropsType) {
     }
   };
   return (
-    <div className="flex justify-center items-center bg-black">
-      <div className="bg-blue-300">
-        <div className="flex items-center justify-center bg-red-300">
-          <div className="flex-col justify-center items-center bg-yellow-300">
-            <div className="flex justify-center items-center mx-16">
+    <div className="flex justify-center items-center">
+      <div className="w-2/3">
+        <div className="flex items-center justify-center w-ful">
+          <div className="flex flex-col lg:flex-row justify-center items-center w-full mt-6">
+            <div className="flex flex-1 justify-center items-center lg:mr-8 ">
               {data[0] && (
                 <Image
                   src={data[0].img}
                   alt="메인 이미지"
                   width={500}
                   height={400}
-                  className="rounded-3xl border-2 my-10"
+                  className="rounded-3xl border-2 w-full h-full"
                 />
               )}
             </div>
-            <div className="flex justify-center items-center my-12 md:my-6">
-              <div className="flex justify-center items-center w-1/2 md:w-full">
-                {data[0] && (
-                  <div className="text-black">
-                    <div className="flex justify-end items-center">
-                      <div
-                        onClick={() => handleFavorite(id)}
-                        className="border-[1px] border-black"
-                      >
-                        {favorites.includes(id) ? (
-                          <FaHeart className="w-8 h-8 m-2 font-light text-main-pink" />
-                        ) : (
-                          <FaRegHeart className="w-8 h-8 m-2 font-light " />
-                        )}
-                      </div>
-                      <div className="border-[1px] border-black ml-1">
-                        <MdShare className="w-8 h-8 m-2 font-light text-black" />
-                      </div>
+            <div className="flex flex-1 w-full h-full">
+              {data[0] && (
+                <div className="text-black w-full">
+                  <div className="flex justify-end items-center my-2">
+                    <div
+                      onClick={() => handleFavorite(id)}
+                      className="border-[1px] border-black p-2"
+                    >
+                      {favorites.includes(id) ? (
+                        <FaHeart className="w-8 h-8 font-light text-main-pink" />
+                      ) : (
+                        <FaRegHeart className="w-8 h-8  font-light " />
+                      )}
                     </div>
-                    <div className="text-5xl font-bold mb-4 leading-normal">
-                      {data[0].name}
-                    </div>
-                    <div className="w-full h-1 bg-gray-100 mb-7"></div>
-                    <div className="mb-20">
-                      <div className="text-2xl flex justify-end mb-7">
-                        {data[0].start} ~ {data[0].end}
-                      </div>
-                      <div className="mt-10 text-lg">
-                        <div className="mb-4">
-                          <span className=" font-semibold">장소 : </span>
-                          {data[0].place}
-                        </div>
-                        <div className="mb-4">
-                          <span className=" font-semibold">장르 : </span>
-                          {data[0].genre}
-                        </div>
-                        <div className="mb-4">
-                          <span className=" font-semibold">
-                            {data[0].prfstate}
-                          </span>
-                        </div>
-                        <div className="mb-4">
-                          <span className=" font-semibold">
-                            공연 소요시간 :{}
-                          </span>
-                          {data[0].runtime}
-                        </div>
-                        <div className="mb-4">
-                          <span className=" font-semibold">연령 : </span>
-                          {data[0].age}
-                        </div>
-                        <div>
-                          <span className=" font-semibold">가격 : </span>
-                          {data[0].price}
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="bg-deep-blue font-semibold text-xl text-white w-full h-16 flex justify-center items-center">
-                      예매하러 가기
+                    <div className="border-[1px] p-2 border-black ml-1">
+                      <MdShare className="w-8 h-8 font-light text-black" />
                     </div>
                   </div>
-                )}
-              </div>
+                  <div className="text-2xl md:text-3xl lg:text-2xl xl:text-3xl 2xl:text-5xl font-bold mb-4 leading-normal">
+                    {data[0].name}
+                  </div>
+                  <div className="w-full h-1 bg-gray-100 mb-7"></div>
+                  <div className="lg:mb-10 2xl:mb-10 3xl:mb-20">
+                    <div className="xl:text-xl 2xl:text-2xl flex justify-end mb-7">
+                      {data[0].start} ~ {data[0].end}
+                    </div>
+                    <div className="mt-10 text-lg 2xl:text-xl">
+                      <div className="mb-4">
+                        <span className=" font-semibold">장소 : </span>
+                        {data[0].place}
+                      </div>
+                      <div className="mb-4">
+                        <span className=" font-semibold">장르 : </span>
+                        {data[0].genre}
+                      </div>
+                      <div className="mb-4">
+                        <span className=" font-semibold">
+                          {data[0].prfstate}
+                        </span>
+                      </div>
+                      <div className="mb-4">
+                        <span className=" font-semibold">
+                          공연 소요시간 :{}
+                        </span>
+                        {data[0].runtime}
+                      </div>
+                      <div className="mb-4">
+                        <span className=" font-semibold">연령 : </span>
+                        {data[0].age}
+                      </div>
+                      <div>
+                        <span className=" font-semibold">가격 : </span>
+                        {data[0].price}
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="bg-deep-blue font-semibold text-xl text-white w-full h-16 flex justify-center items-center">
+                    예매하러 가기
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
         <div className="flex justify-center items-center mx-16 my-20">
-          <div className="flex justify-center items-center w-2/3 h-16 ">
+          <div className="flex justify-center items-center w-full h-16 ">
             <div className="border-[1px] border-x-black border-t-black border-b-0 w-1/2 h-full ">
               <Link
                 href="#detail"
@@ -174,8 +172,8 @@ function DetailPage(props: PropsType) {
             </div>
           </div>
         </section>
-        <div className="flex justify-center items-center mx-16 my-20">
-          <div className="flex justify-center items-center w-2/3 h-16 ">
+        <div className="flex justify-center items-center md:mx-16 my-20">
+          <div className="flex justify-center items-center w-full h-16">
             <div className="border-[1px] w-1/2 h-full border-b-black bg-review-section ">
               <Link
                 href="#detail"
@@ -199,8 +197,8 @@ function DetailPage(props: PropsType) {
           </div>
         </div>
         <section id="review">
-          <div className="flex justify-center items-center mx-16">
-            <div className="bg-black w-2/3 flex-col mt-20 h-80 px-10 justify-center items-center">
+          <div className="flex justify-center items-center xl:mx-16">
+            <div className="bg-black w-full flex-col mt-20 px-10 justify-center items-center">
               <div className="flex justify-center items-center">
                 <div className="w-full h-1/3">
                   <div className="flex justify-start items-center">
@@ -210,17 +208,17 @@ function DetailPage(props: PropsType) {
                   </div>
                 </div>
               </div>
-              <div className="flex justify-center items-center bg-white">
-                <div className="w-2/3 h-2/3">
-                  <div className="flex items-center justify-center">
+              <div className="flex justify-center items-center w-full">
+                <div className="w-full h-2/3">
+                  <div className="flex items-center justify-center w-full">
                     <CreateReview id={id} />
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="flex justify-center items-center mx-16">
-            <div className="bg-black w-2/3">
+          <div className="flex justify-center items-center xl:mx-16">
+            <div className="bg-black w-full">
               <div className="font-semibold text-3xl my-10 text-white mx-9">
                 리뷰(개수)
               </div>
