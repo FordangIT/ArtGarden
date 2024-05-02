97a0c7c41603aa2fd366b1b39c9643575412ea25
FordangIT <fordang0819@gmail.com>
2024-05-02 19:20:03 +0900

    [AG-116]Feat: 리뷰 SSG 렌더링

--- lib/loadData.tsx
+++ lib/loadData.tsx
@@ -1,3 +1,19 @@
+import { useQuery } from "react-query";
+import axios, { AxiosResponse } from "axios";
+interface Review_Full {
+  data: Review_Data[];
+}
+interface Review_Data {
+  name: string;
+  reviewid: string;
+  performid: string;
+  content: string;
+  rate: number;
+  regdt: string;
+  membierid: string;
+  genre: string;
+  posterurl: string;
+}
 export async function loadNew() {
   // Call an external API endpoint to get posts
   const res = await fetch("http://localhost:3060/api/performances/new");
@@ -14,6 +30,13 @@ export async function loadBest() {
     const result = await response.json();
     return result;
   } catch (error) {
-    console.error("error fetching datat", error);
+    console.error("error fetching data", error);
+    throw new Error("failed to fetch data");
   }
+}
+
+export async function loadReview() {
+  const res = await fetch(`https://artgarden.site/reviews?page=1&size=8`);
+  const data = await res.json();
+  return data;
 }
