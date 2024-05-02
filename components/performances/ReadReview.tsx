6e9173bc8a67392e0d815341fbed0b39301acf84
FordangIT <fordang0819@gmail.com>
2024-05-02 19:18:42 +0900

    ETC: 수정, 삭제 코드 주석 처리

--- components/performances/ReadReview.tsx
+++ components/performances/ReadReview.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useQuery, useMutation, useQueryClient } from "react-query";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
-import UpdateReview from "./UpdateReview"; // UpdateReview 컴포넌트를 가져옵니다.
-
 interface ReviewFormProps {
   id: string;
 }
@@ -55,27 +53,27 @@ export function ReadReview({ id }: ReviewFormProps) {
   const [editingReviewId, setEditingReviewId] = useState<number | null>(null);
   const [newReviewContent, setNewReviewContent] = useState<string>("");
 
-  const updateMutation = useMutation(updateReview);
-  const deletemutation = useMutation(deleteReview);
+  // const updateMutation = useMutation(updateReview);
+  // const deletemutation = useMutation(deleteReview);
 
-  const handleUpdate = async (reviewId: number) => {
-    try {
-      await updateMutation.mutateAsync(reviewId, newReviewContent);
-      setEditingReviewId(null);
-      console.log("리뷰가 성공적으로 업데이트되었습니다.");
-    } catch (error) {
-      console.error("리뷰 업데이트에 실패했습니다:", error);
-    }
-  };
+  // const handleUpdate = async (reviewId: number) => {
+  //   try {
+  //     await updateMutation.mutateAsync(reviewId, newReviewContent);
+  //     setEditingReviewId(null);
+  //     console.log("리뷰가 성공적으로 업데이트되었습니다.");
+  //   } catch (error) {
+  //     console.error("리뷰 업데이트에 실패했습니다:", error);
+  //   }
+  // };
 
-  const handleDelete = async (reviewId: number) => {
-    try {
-      await deletemutation.mutateAsync(reviewId);
-      console.log("리뷰가 성공적으로 삭제되었습니다.");
-    } catch (error) {
-      console.error("리뷰 삭제에 실패했습니다:", error);
-    }
-  };
+  // const handleDelete = async (reviewId: number) => {
+  //   try {
+  //     await deletemutation.mutateAsync(reviewId);
+  //     console.log("리뷰가 성공적으로 삭제되었습니다.");
+  //   } catch (error) {
+  //     console.error("리뷰 삭제에 실패했습니다:", error);
+  //   }
+  // };
 
   const fetchData = async (pageNum = 1): Promise<{ data: ReviewData[] }> => {
     const res = await fetch(
@@ -129,7 +127,7 @@ export function ReadReview({ id }: ReviewFormProps) {
                     />
                   ))}
                 </div>
-                {editingReviewId === el.reviewid ? (
+                {/* {editingReviewId === el.reviewid ? (
                   <div className="py-2 flex justify-between">
                     <input
                       type="text"
@@ -164,10 +162,13 @@ export function ReadReview({ id }: ReviewFormProps) {
                       </div>
                     </div>
                   </div>
-                )}
-                {editingReviewId === el.reviewid && ( // 리뷰를 수정할 때만 UpdateReview 컴포넌트를 보여줍니다.
+                )} */}
+                {/* {editingReviewId === el.reviewid && ( // 리뷰를 수정할 때만 UpdateReview 컴포넌트를 보여줍니다.
                   <UpdateReview reviewId={el.reviewid} />
-                )}
+                )} */}
+                <div className="py-2 flex justify-between">
+                  <div>리뷰: {el.content}</div>
+                </div>
               </div>
             ))}
         </div>
