c6ec05bd939578b942d162883832761c9034420e
FordangIT <fordang0819@gmail.com>
2024-05-12 21:50:04 +0900

    Design: drawer css

--- components/basic/Navbar.tsx
+++ components/basic/Navbar.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaArrowDown } from "react-icons/fa";
 import Smallbar from "./Smallbar";
 import Sidebar from "./Sidebar";
+import { signIn, signOut, useSession } from "next-auth/react";
 export default function Navbar() {
+  const { data: session, status } = useSession();
   return (
     <>
       <Sidebar />
-      <div className="bg-main-pink flex justify-center items-center h-20 lg:h-40 fixed to-0 w-full z-40">
+      <div className="bg-main-pink flex justify-center items-center h-16 lg:h-20 w-full z-40">
         <div className="flex justify-between items-center w-2/3">
           <Link href="/" className="w-full h-full">
             <Image
@@ -15,7 +16,7 @@ export default function Navbar() {
               alt="logo of artgarden"
               width={200}
               height={40}
-              className="w-48"
+              className="w-36 lg:w-48"
             ></Image>
           </Link>
           <div className="lg:hidden">
@@ -23,7 +24,7 @@ export default function Navbar() {
           </div>
           <div className="hidden lg:block">
             <div className="flex">
-              <div className="flex items-center justify-between font-semibold text-2xl text-white w-72 ml-8">
+              <div className="flex items-center justify-end gap-x-4 font-semibold text-lg text-white w-72 ml-8">
                 <Link href="/performances">
                   <div className="hover:text-main-yellow ">공연</div>
                 </Link>
@@ -36,10 +37,13 @@ export default function Navbar() {
               </div>
               <div className="flex justify-around items-center bg-black w-36 h-12 border-2 border-main-yellow rounded-2xl ml-12">
                 <div className="font-semibold text-xl text-main-yellow">
-                  Login
-                </div>
-                <div className="bg-main-yellow rounded-full text-lg">
-                  <FaArrowDown />
+                  {session ? (
+                    <button onClick={() => signOut()}>Log Out</button>
+                  ) : (
+                    <Link href="/auth/signin">
+                      <button>Login</button>
+                    </Link>
+                  )}
                 </div>
               </div>
             </div>
