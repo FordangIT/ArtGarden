e0028ba80668c1455c39672ea8d0e7775a21a81e
FordangIT <fordang0819@gmail.com>
2024-05-20 21:48:43 +0900

    Design: 마이페이지 추가

--- components/basic/Navbar.tsx
+++ components/basic/Navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Smallbar from "./Smallbar";
 import Sidebar from "./Sidebar";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { FaUserAlt } from "react-icons/fa";
 export default function Navbar() {
   const { data: session, status } = useSession();
   return (
@@ -35,17 +36,28 @@ export default function Navbar() {
                   <div className="hover:text-main-yellow">팝업스토어</div>
                 </Link>
               </div>
-              <div className="flex justify-around items-center bg-black w-36 h-12 border-2 border-main-yellow rounded-2xl ml-12">
-                <div className="font-semibold text-xl text-main-yellow">
-                  {session ? (
-                    <button onClick={() => signOut()}>Log Out</button>
-                  ) : (
-                    <Link href="/auth/signin">
-                      <button>Login</button>
-                    </Link>
-                  )}
+              <div className="flex justify-center items-center">
+                <div className="flex justify-around items-center bg-black w-32 h-10 border-2 border-main-yellow rounded-2xl ml-12 mr-4">
+                  <div className="font-semibold text-xl text-main-yellow">
+                    {session ? (
+                      <button onClick={() => signOut()}>Log Out</button>
+                    ) : (
+                      <Link href="/auth/signin">
+                        <button>Login</button>
+                      </Link>
+                    )}
+                  </div>
                 </div>
               </div>
+              {session ? (
+                <Link href="/user/saveitems">
+                  <div className="flex justify-center items-center text-white text-2xl m-4">
+                    <FaUserAlt />
+                  </div>
+                </Link>
+              ) : (
+                <></>
+              )}
             </div>
           </div>
         </div>
