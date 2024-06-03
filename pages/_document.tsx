80b09eca7547a920ae6a6b2559fd2421aca7d818
FordangIT <fordang0819@gmail.com>
2024-06-03 16:10:33 +0900

    Feat: 카카오톡 공유 기능

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script
+          defer
+          src="https://developers.kakao.com/sdk/js/kakao.min.js"
+        ></script>
         <link rel="icon" href="/artgarden.png" sizes="any" />
       </Head>
       <body>
