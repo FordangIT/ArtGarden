40587d90cdc18ca6c2d4258de6b0919ba514080c
FordangIT <fordang0819@gmail.com>
2024-06-03 16:16:10 +0900

    Optimization: util 함수 분리

---
+++ lib/components/TruncateText.tsx
@@ -0,0 +1,6 @@
+export const truncateText = (text: string, maxLength: number) => {
+  if (!text) {
+    return "";
+  }
+  return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
+};
