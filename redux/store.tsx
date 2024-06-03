6cf575e18822f678b4f2938455d4db37fe07350a
FordangIT <fordang0819@gmail.com>
2024-06-03 16:23:59 +0900

    Feat: 전시회 찜 상태 전역 관리

--- redux/store.tsx
+++ redux/store.tsx
@@ -2,10 +2,12 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { selectSlice } from "./slices/selectSlice";
 import favoriteReducer from "./slices/favoriteSlice";
 import performanceSlice from "./slices/performanceSlice";
+import exhibitionSlice from "./slices/exhibitionSlice";
 const rootReducer = combineReducers({
   selected: selectSlice.reducer,
   favorites: favoriteReducer,
-  performance: performanceSlice
+  performance: performanceSlice,
+  exhibition: exhibitionSlice
 });
 
 const store = configureStore({
