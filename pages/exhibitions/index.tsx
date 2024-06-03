0c2f4d261b988d6e8938b681178a1229c03edb7d
FordangIT <fordang0819@gmail.com>
2024-06-03 16:18:57 +0900

    Feat: 모든 전시회 페이지 추가

--- pages/exhibitions/index.tsx
+++ pages/exhibitions/index.tsx
@@ -1,14 +1,9 @@
+import AllExhibitions from "@/components/exhibitions/AllExhibitions";
 import Image from "next/image";
 function Exhibition() {
   return (
-    <div className="flex justify-center items-center my-44 ">
-      <Image
-        src="/readyPage.png"
-        alt="준비중 페이지"
-        width={100}
-        height={100}
-        className="w-96 h-96"
-      />
+    <div className="flex-col">
+      <AllExhibitions />
     </div>
   );
 }
