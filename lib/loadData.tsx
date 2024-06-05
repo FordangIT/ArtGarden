8a1c29dd0f21a542d58395ddd66d1e8e0d042410
FordangIT <fordang0819@gmail.com>
2024-06-05 15:44:42 +0900

    Modify: 타입 변경 및 데이터 에러 처리

--- lib/loadData.tsx
+++ lib/loadData.tsx
@@ -1,35 +1,24 @@
-import { useQuery } from "react-query";
-import axios, { AxiosResponse } from "axios";
-import { ObjectId } from "mongodb";
 import clientPromise from "../lib/mongodb";
-
-interface Review_Full {
-  data: Review_Data[];
-}
-interface Review_Data {
-  name: string;
-  reviewid: string;
-  performid: string;
-  content: string;
-  rate: number;
-  regdt: string;
-  membierid: string;
-  genre: string;
-  posterurl: string;
-}
-export async function loadNew() {
-  // Call an external API endpoint to get posts
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/performances/new`
-  );
-  const data = await res.json();
-  return data;
-}
 
 export async function loadBest() {
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/performances/best`
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/performances?status=all&days=30&page=1&size=9&orderby=popular`
+    );
+    if (!response.ok) {
+      throw new Error("failed to fetch data");
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("error fetching data", error);
+    throw new Error("failed to fetch data");
+  }
+}
+export async function loadNew() {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/performances?status=all&days=30&page=1&size=9&orderby=latest`
     );
     if (!response.ok) {
       throw new Error("failed to fetch data");
@@ -42,20 +31,20 @@ export async function loadBest() {
   }
 }
 
-export async function loadReview() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/reviews?page=1&size=8`
-  );
-  const data = await res.json();
-  return data.data;
-}
 export async function loadBestExhibit() {
-  // Call an external API endpoint to get posts
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/exhibits?days=30&page=1&size=9&orderby=popular`
-  );
-  const data = await res.json();
-  return data;
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/exhibits?days=30&page=1&size=9&orderby=popular`
+    );
+    if (!response.ok) {
+      throw new Error("failed to fetch data");
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("error fetching data", error);
+    throw new Error("failed to fetch data");
+  }
 }
 
 export async function loadNewExhibit() {
@@ -96,4 +85,20 @@ export async function loadBestPopup() {
     _id: item._id.toString()
   }));
   return bestPopup;
+}
+
+export async function loadReview() {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/reviews?page=1&size=8`
+    );
+    if (!response.ok) {
+      throw new Error("failed to fetch data");
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("error fetching data", error);
+    throw new Error("failed to fetch data");
+  }
 }
