2f01258d3d5fba21e7b81d7016ce4f2817b666b4
FordangIT <fordang0819@gmail.com>
2024-06-07 12:52:19 +0900

    Feat: 구글 ads 연동

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script"; // Import Next.js Script component
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <script
-          defer
-          src="https://developers.kakao.com/sdk/js/kakao.min.js"
-        ></script> */}
+        {/* Add the Google AdSense script using Next.js Script component */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6499853936822237"
+          crossOrigin="anonymous" // Correct attribute name
+        />
         <link rel="icon" href="/artgarden.png" sizes="any" />
       </Head>
       <body>
