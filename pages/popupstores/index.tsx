758e9b72c78ebdfaa5cfc80c3885a3dae43cb0bb
FordangIT <fordang0819@gmail.com>
2024-06-10 21:46:31 +0900

    Feat: 팝업스토어 기본 페이지 생성

--- pages/popupstores/index.tsx
+++ pages/popupstores/index.tsx
@@ -1,15 +1,25 @@
-import Image from "next/image";
-function PopupStores() {
+import AllPopupStores from "@/components/popupstores/AllPopupStores";
+import { GetServerSideProps } from "next";
+import { loadAllPopupStore } from "@/lib/loadData";
+import { useEffect } from "react";
+import { AllPopupStore_TYPE } from "@/pages";
+function PopupStores(props: AllPopupStore_TYPE) {
+  useEffect(() => {
+    console.log(props);
+  });
   return (
-    <div className="flex justify-center items-center my-44 ">
-      <Image
-        src="/readyPage.png"
-        alt="준비중 페이지"
-        width={100}
-        height={100}
-        className="w-96 h-96"
-      />
+    <div className="flex-col ">
+      <AllPopupStores data={props.data} />
     </div>
   );
 }
 export default PopupStores;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const allPopupStores = await loadAllPopupStore();
+  return {
+    props: {
+      data: allPopupStores
+    }
+  };
+};
