ddbc30f06b11cf68e5f672219ed6c5f2af4a8d1b
FordangIT <fordang0819@gmail.com>
2024-06-12 18:24:56 +0900

    Docs: 메타 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,6 +37,10 @@ export default function App({
                 name="google-adsense-account"
                 content="ca-pub-6499853936822237"
               ></meta>
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+              />
             </Head>
             <Component {...pageProps} />
           </Layout>
