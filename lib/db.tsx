a865561b816871b241932d4ab877f634423f6c8e
FordangIT <fordang0819@gmail.com>
2024-06-19 09:41:58 +0900

    Remove: 파일 구조 이동

--- lib/db.tsx
+++
@@ -1,9 +0,0 @@
-//mongodb에 연결하기.
-import { MongoClient } from "mongodb";
-
-export async function connectToDatabase() {
-  const client = await MongoClient.connect(
-    "mongodb+srv://fo_rdang:skfro100!@atlascluster.l3c4pyr.mongodb.net/"
-  );
-  return client;
-}
