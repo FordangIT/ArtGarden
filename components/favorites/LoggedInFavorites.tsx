92217a67a5378965300b01889545231fc427a742
FordangIT <fordang0819@gmail.com>
2024-06-25 19:56:54 +0900

    Rename: 데이터 접근 키 변경

--- components/favorites/LoggedInFavorites.tsx
+++ components/favorites/LoggedInFavorites.tsx
@@ -11,7 +11,7 @@ import { SaveItems, ExSaveItems } from "./LoggedOutFavorites";
 import { NosaveItems } from "@/lib/components/NosaveItems";
 
 interface ScrapResponse {
-  myDTOList: { objectid: string; scrapyn: boolean }[];
+  datalist: { objectid: string; scrapyn: boolean }[];
 }
 
 export default function LoggedInFavorites() {
@@ -26,7 +26,7 @@ export default function LoggedInFavorites() {
   // 비동기 작업을 처리할 함수 정의
   const fetchData = async () => {
     try {
-      const favoriteIds = data?.myDTOList || [];
+      const favoriteIds = data?.datalist || [];
       const validFavoriteIds = favoriteIds.map((item) => item.objectid);
       const exIds = validFavoriteIds.filter((id) => id.startsWith("EX"));
       const peIds = validFavoriteIds.filter((id) => id.startsWith("PF"));
