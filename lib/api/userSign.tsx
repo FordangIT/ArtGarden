be1f09ac252628ae92e190767dea2d2cd83cf970
FordangIT <fordang0819@gmail.com>
2024-06-30 23:09:19 +0900

    Fix: 데이터 변경

--- lib/api/userSign.tsx
+++ lib/api/userSign.tsx
@@ -1,11 +1,16 @@
 import axios from "axios";
+interface OauthLoginData_TYPE {
+  loginid: string;
+  name: string;
+  email: string;
+}
 //사용자 소셜 로그인할 때 백엔드한테 id 보내기
-export const postUserId = async (userId: number | string) => {
+export const postUserId = async (loginData: OauthLoginData_TYPE) => {
   try {
     const response = await axios.post(
       `${process.env.NEXT_PUBLIC_BACKEND_URL}/oauthLoginProcess`,
       {
-        loginid: String(userId)
+        loginData
       },
       {
         withCredentials: true // 쿠키를 포함한 요청 설정
@@ -15,31 +20,9 @@ export const postUserId = async (userId: number | string) => {
     if (response.status !== 200) {
       throw new Error("Failed to post user ID");
     }
-
     console.log("User ID posted successfully");
   } catch (error) {
     console.error("Error posting user ID:", error);
-  }
-};
-
-// 회원정보상세 조회
-export const getMemberDetails = async () => {
-  try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/members`,
-      {
-        withCredentials: true // 쿠키를 포함한 요청 설정
-      }
-    );
-
-    if (response.status !== 200) {
-      throw new Error("Failed to get member details");
-    }
-
-    console.log("Member details retrieved successfully:", response.data);
-    return response.data; // 필요한 데이터를 반환
-  } catch (error) {
-    console.error("Error getting member details:", error);
   }
 };
 
@@ -81,30 +64,7 @@ export const checkLoginId = async (loginId: string) => {
   }
 };
 
-export const updateMemberInfo = async (memberInfo: any) => {
-  try {
-    const response = await axios.patch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/members`,
-      memberInfo,
-      {
-        headers: {
-          "Content-Type": "application/json"
-        },
-        withCredentials: true // 쿠키를 포함한 요청 설정
-      }
-    );
-
-    if (response.status !== 200) {
-      throw new Error("Failed to update member info");
-    }
-
-    console.log("Member info updated successfully:", response.data);
-    return response.data; // 필요한 데이터를 반환
-  } catch (error) {
-    console.error("Error updating member info:", error);
-  }
-};
-
+//login 유저
 export const loginUser = async (loginInfo: any) => {
   try {
     const response = await axios.post(
@@ -125,31 +85,6 @@ export const loginUser = async (loginInfo: any) => {
     return response.data; // 필요한 데이터를 반환
   } catch (error) {
     console.error("Error logging in:", error);
-  }
-};
-
-// 회원 탈퇴
-export const leaveMember = async (loginid: any) => {
-  try {
-    const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/leaveId`,
-      { loginid: loginid },
-      {
-        headers: {
-          "Content-Type": "application/json"
-        },
-        withCredentials: true // 쿠키를 포함한 요청 설정
-      }
-    );
-
-    if (response.status !== 200) {
-      throw new Error("Failed to leave member");
-    }
-
-    console.log("Member left successfully:", response.data);
-    return response.data; // 필요한 데이터를 반환
-  } catch (error) {
-    console.error("Error leaving member:", error);
   }
 };
 
