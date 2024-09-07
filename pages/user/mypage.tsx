ae983730d63f4d3c26ca0b44c8a74450162cb0c6
FordangIT <fordang0819@gmail.com>
2024-09-07 21:51:22 +0900

    Fix: 닉네임 초기 업데이트 수정

--- pages/user/mypage.tsx
+++ pages/user/mypage.tsx
@@ -10,10 +10,11 @@ import { logOut } from "@/redux/slices/checkLoginSlice";
 import { signOut } from "next-auth/react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/redux/store";
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import * as yup from "yup";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+
 interface MemberDetails {
   name: string;
   loginid: string;
@@ -39,28 +40,39 @@ export default function MyPage() {
   const queryClient = useQueryClient();
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state: RootState) => state.login.isLoggedIn);
-  const { data, error, isLoading } = useQuery<MemberDetails>(
-    "memberDetails",
-    getMemberDetails
-  );
   const [readOnlyStatus, setReadOnly] = useState<boolean>(true);
   const [buttonName, setButtonName] = useState<string>("변경");
 
+  const { data, error, isLoading } = useQuery<MemberDetails>(
+    "memberDetails",
+    getMemberDetails,
+    {
+      onSuccess: (memberData) => {
+        reset(memberData);
+      }
+    }
+  );
+  const [nickname, setNickName] = useState(data?.nickname);
   const {
     register,
+    reset,
     handleSubmit,
     getValues,
     setError,
+    setFocus,
     clearErrors,
     formState: { errors }
   } = useForm<UpdateUserNickname>({
     resolver: yupResolver(schema),
-    mode: "onSubmit",
-    defaultValues: { nickname: data?.nickname || "" }
+    mode: "onChange",
+    defaultValues: { nickname }
   });
 
   useEffect(() => {
-    if (data) clearErrors();
+    if (data) {
+      clearErrors();
+      setNickName(data.nickname);
+    }
   }, [data, clearErrors]);
 
   if (isLoading) {
@@ -78,6 +90,10 @@ export default function MyPage() {
   if (!isLoggedIn) {
     router.push(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/auth/signin`);
   }
+
+  const handleChange = (value: string) => {
+    setNickName(value);
+  };
 
   const handleClick = async (loginid: string) => {
     try {
@@ -100,19 +116,19 @@ export default function MyPage() {
 
   const handleButton = async () => {
     if (buttonName === "완료") {
-      handleSubmit(async ({ nickname }) => {
-        try {
-          await updateMemberInfo({ loginid: data!.loginid, nickname });
-          queryClient.invalidateQueries("memberDetails");
-          setReadOnly(true);
-          setButtonName("변경");
-        } catch (error) {
-          setError("nickname", {
-            type: "manual",
-            message: "닉네임 변경에 실패했습니다. 다시 시도해 주세요."
-          });
-        }
-      })();
+      const newNickName = getValues("nickname");
+      try {
+        await updateMemberInfo({ loginid: data?.loginid, newNickName });
+        queryClient.invalidateQueries("memberDetails");
+        setReadOnly(true);
+        setButtonName("변경");
+      } catch (error) {
+        setError("nickname", {
+          type: "manual",
+          message: "닉네임 변경에 실패했습니다. 다시 시도해 주세요."
+        });
+        setFocus("nickname");
+      }
     } else {
       setReadOnly(false);
       setButtonName("완료");
@@ -155,27 +171,37 @@ export default function MyPage() {
               <div className="flex flex-row justify-center items-center py-2 border-b-[1px] border-slate-300">
                 <div className="basis-1/4 font-semibold">닉네임</div>
                 <div className="basis-3/4 flex justify-end lg:justify-start items-center">
-                  <input
-                    type="text"
-                    {...register("nickname")}
-                    readOnly={readOnlyStatus}
-                    className={`w-full max-w-[11rem] py-2 mr-2 ${
-                      readOnlyStatus
-                        ? "outline-none bg-white"
-                        : "border-slate-900"
-                    } text-black`}
-                  />
-                  <button
-                    type="button"
-                    className={`px-3 py-2 text-sm cursor-pointer h-full ${
-                      buttonName === "변경"
-                        ? "bg-slate-100 text-black"
-                        : "bg-blue-600 text-white font-medium"
-                    }`}
-                    onClick={handleButton}
-                  >
-                    {buttonName}
-                  </button>
+                  <form onSubmit={handleSubmit(handleButton)}>
+                    <input
+                      {...register("nickname", {
+                        pattern: {
+                          value: /^[가-힣a-zA-Z0-9]{2,10}$/,
+                          message: "2~10자 한글, 영문, 숫자 형식"
+                        }
+                      })}
+                      readOnly={readOnlyStatus}
+                      type="text"
+                      className={`w-full max-w-[11rem] py-2 mr-2 ${
+                        readOnlyStatus
+                          ? "outline-none bg-white"
+                          : "border-slate-900"
+                      } text-black`}
+                    />
+                    <button
+                      type="submit"
+                      className={`px-3 py-2 text-sm cursor-pointer h-full ${
+                        buttonName === "변경"
+                          ? "bg-slate-100 text-black"
+                          : "bg-blue-600 text-white font-medium"
+                      }`}
+                      onClick={handleButton}
+                    >
+                      {buttonName}
+                    </button>
+                    {errors?.nickname ? (
+                      <p>{errors.nickname?.message}</p>
+                    ) : null}
+                  </form>
                 </div>
               </div>
               {errors.nickname && (
