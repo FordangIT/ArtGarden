b26d5b408bb168bc7a47536c02852b5ef9175ffb
FordangIT <fordang0819@gmail.com>
2024-09-13 19:45:17 +0900

    Chore: redux-persist 설치

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
 import { selectSlice } from "./slices/selectSlice";
 import favoriteReducer from "./slices/favoriteSlice";
 import performanceSlice from "./slices/performanceSlice";
 import exhibitionSlice from "./slices/exhibitionSlice";
 import modalSlice from "./slices/modalSlice";
 import checkLoginSlice from "./slices/checkLoginSlice";
+
 const rootReducer = combineReducers({
   login: checkLoginSlice,
   selected: selectSlice.reducer,
